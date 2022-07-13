326a2e4556b9527a9d539a057f216e14dd85e29e
Marvin0555 <marcos.vinicius@oceanbrasil.com>
2022-07-13 13:20:40 -0400

    Update do código do braço

--- braco_robotico_da_esteira/braco_robotico_da_esteira.ino
+++ braco_robotico_da_esteira/braco_robotico_da_esteira.ino
@@ -1,26 +1,28 @@
-
-
 #include <ESP32Servo.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
-Servo myservo1;
-Servo myservo2;
-Servo myservo3;
-Servo myservo4;
-Servo myservo5;// create servo object to control a servo
+
+Servo eixo1;
+Servo eixo2;
+Servo eixo3;
+Servo eixo4;
+Servo eixo5;
+Servo eixo6; // create servo object to control a servo
 
 String msg;
 
 const char* ssid = "Ocean_Adm";
 const char* password = "@artico2020!";
 
-const char* topic_sub1 = "arm-ocean-1";
-const char* topic_sub2 = "arm-ocean-2";
-const char* topic_sub3 = "arm-ocean-3";
-const char* topic_sub4 = "arm-ocean-4";
-const char* topic_sub5 = "arm-ocean-5";
+const char* topic_sub1 = "eixo1-ocean";
+const char* topic_sub2 = "eixo2-ocean";
+const char* topic_sub3 = "eixo3-ocean";
+const char* topic_sub4 = "eixo4-ocean";
+const char* topic_sub5 = "eixo5-ocean";
+const char* topic_sub6 = "eixo6-ocean";
+
 //const char* topic_pub = "laps-ocean-iot-1";
-const char* mqtt_id = "esp-44";
+const char* mqtt_id = "esp32-ocean";
 
 const char* BROKER_MQTT = "mqtt.eclipseprojects.io"; //URL do broker MQTT que se deseja utilizar
 int BROKER_PORT = 1883; // Porta do Broker MQTT
@@ -51,25 +53,29 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length)
     }
     Serial.println("Chegou a seguinte string via MQTT: ");
     Serial.println(topic);
-    if(String(topic) == "arm-ocean-1"){
-       myservo1.write(msg.toInt());
+    if(String(topic) == topic_sub1){
+       eixo1.write(msg.toInt());
        Serial.println(msg);
        //delay(15);
     }
     else if(String(topic) == topic_sub2){
-       myservo2.write(msg.toInt());
+       eixo2.write(msg.toInt());
        Serial.println(msg);
     }
-    else if(String(topic) =="arm-ocean-3"){
-       myservo3.write(msg.toInt());
+    else if(String(topic) == topic_sub3){
+       eixo3.write(msg.toInt());
        Serial.println(msg);
     }
-    else if(String(topic) =="arm-ocean-4"){
-       myservo4.write(msg.toInt());
+    else if(String(topic) == topic_sub4){
+       eixo4.write(msg.toInt());
        Serial.println(msg);
     }
-    else if(String(topic) =="arm-ocean-5"){
-       myservo5.write(msg.toInt());
+    else if(String(topic) == topic_sub5){
+       eixo5.write(msg.toInt());
+       Serial.println(msg);
+    }
+    else if(String(topic) == topic_sub6){
+       eixo6.write(msg.toInt());
        Serial.println(msg);
     }
 }
@@ -88,6 +94,7 @@ void reconnectMQTT(void)
             MQTT.subscribe(topic_sub3);
             MQTT.subscribe(topic_sub4);
             MQTT.subscribe(topic_sub5);
+            MQTT.subscribe(topic_sub6);
         }
         else
         {
@@ -102,7 +109,6 @@ void VerificaConexaoMQTT(void)
 {
     if (!MQTT.connected())
         reconnectMQTT(); //se não há conexão com o Broker, a conexão é refeita
-     
 }
  
 
@@ -117,26 +123,22 @@ void setup() {
   Serial.println("WiFi Connected....IP Address:");
   Serial.println(WiFi.localIP());
 
-  myservo1.setPeriodHertz(50);    
-  myservo1.attach(2); 
-  myservo2.setPeriodHertz(50);    
-  myservo2.attach(4); 
-  myservo3.setPeriodHertz(50);    
-  myservo3.attach(5); 
-  myservo4.setPeriodHertz(50);    
-  myservo4.attach(19);
-  myservo5.setPeriodHertz(50);    
-  myservo5.attach(21);
+  eixo1.setPeriodHertz(50);    
+  eixo1.attach(2); 
+  eixo2.setPeriodHertz(50);    
+  eixo2.attach(4); 
+  eixo3.setPeriodHertz(50);    
+  eixo3.attach(5); 
+  eixo4.setPeriodHertz(50);    
+  eixo4.attach(18);
+  eixo5.setPeriodHertz(50);    
+  eixo5.attach(19);
+  eixo6.setPeriodHertz(50);    
+  eixo6.attach(21);
   initMQTT();
 }
 
 void loop() {
-
-
-      VerificaConexaoMQTT();
-
+    VerificaConexaoMQTT();
     MQTT.loop();
-
-
-
 }
